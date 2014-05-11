93f7f40ab74aa8926997cccf16f2d15540a8eec6
Paul Smith <code@uvwxy.de>
2014-05-11 12:39:06 +0200

    misc: refresh baro values every second

--- src/de/uvwxy/swbarowidget/HelloWidget.java
+++ src/de/uvwxy/swbarowidget/HelloWidget.java
@@ -32,60 +32,148 @@ Copyright (c) 2011-2013, Sony Mobile Communications AB
 
 package de.uvwxy.swbarowidget;
 
+import android.content.SharedPreferences.Editor;
+import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 
+import com.sonyericsson.extras.liveware.aef.control.Control;
+import com.sonyericsson.extras.liveware.aef.widget.Widget;
 import com.sonyericsson.extras.liveware.extension.util.widget.BaseWidget;
+
+import de.uvwxy.helper.IntentTools;
+import de.uvwxy.sensors.BarometerReader;
+import de.uvwxy.sensors.SensorReader.SensorResultCallback;
+import de.uvwxy.units.Unit;
 
 /**
  * This demonstrates how to implement a simple text widget.
  */
 class HelloWidget extends BaseWidget {
 
-    public static final int WIDGET_WIDTH_CELLS = 2;
+	public static final int WIDGET_WIDTH_CELLS = 2;
+	public static final int WIDGET_HEIGHT_CELLS = 1;
+	private static final String BARO_WIDGET_SETTINGS = "BARO_SETTINGS";
+	private static final String LAST_VALUE_MB = "LAST_VALE_MB";
+	
+	private SensorResultCallback cb = new SensorResultCallback() {
+		
+		@Override
+		public void result(float[] f) {
+			if (f != null && f.length > 0) {
+				lastValueMB = f[0];
+			}
+		}
+	};
 
-    public static final int WIDGET_HEIGHT_CELLS = 1;
+	private BarometerReader baroReader;
+	private float lastValueMB = 0f;
 
-    /**
-     * Creates a widget extension.
-     */
-    public HelloWidget(WidgetBundle bundle) {
-        super(bundle);
-    }
+	private Handler mHandler = new Handler();
+	private long delayMillis = 1000;
+	private Runnable mUpdateTimeTask = new Runnable() {
 
-    @Override
-    public void onStartRefresh() {
-        Log.d(HelloWidgetExtensionService.LOG_TAG, "startRefresh");
-        // Send a UI when the widget is visible.
-        showLayout(R.layout.layout_widget);
-    }
+		public void run() {
 
-    @Override
-    public void onStopRefresh() {
-        Log.d(HelloWidgetExtensionService.LOG_TAG, "stopRefesh");
-    }
+			// ACTION!
+			updateScreen();
 
-    @Override
-    public void onTouch(final int type, final int x, final int y) {
-        Log.d(HelloWidgetExtensionService.LOG_TAG, "onTouch() " + type);
-    }
+			mHandler.postDelayed(this, delayMillis);
+		}
+	};
+	
+	private void pauseHandler() {
+		mHandler.removeCallbacks(mUpdateTimeTask);
+	}
 
-    @Override
-    public int getWidth() {
-        return (int)(mContext.getResources().getDimension(R.dimen.smart_watch_2_widget_cell_width) * WIDGET_WIDTH_CELLS);
-    }
+	private void unPauseHandler() {
+		mHandler.removeCallbacks(mUpdateTimeTask);
+		mHandler.postDelayed(mUpdateTimeTask, delayMillis);
+	}
+	
+	/**
+	 * Creates a widget extension.
+	 */
+	public HelloWidget(WidgetBundle bundle) {
+		super(bundle);
+	}
 
-    @Override
-    public int getHeight() {
-        return (int)(mContext.getResources().getDimension(R.dimen.smart_watch_2_widget_cell_height) * WIDGET_HEIGHT_CELLS);
-    }
+	@Override
+	public void onStartRefresh() {
+		Log.d(HelloWidgetExtensionService.LOG_TAG, "startRefresh");
 
-    @Override
-    public int getPreviewUri() {
-        return R.drawable.widget_frame;
-    }
+		lastValueMB = IntentTools.getSettings(mContext, BARO_WIDGET_SETTINGS).getFloat(LAST_VALUE_MB, 0);
+		Log.d(HelloWidgetExtensionService.LOG_TAG, "loaded " + lastValueMB);
 
-    @Override
-    public int getName() {
-        return R.string.extension_name;
-    }
+		if (baroReader == null) {
+			baroReader = new BarometerReader(mContext, -1, cb);
+		}
+
+		baroReader.startReading();
+		updateScreen();
+		
+		unPauseHandler();
+	}
+
+	private void updateScreen() {
+		Unit uLastValue = Unit.from(Unit.MILLI_BAR);
+		uLastValue.setValue(lastValueMB);
+
+		Unit uLastHeight = Unit.from(Unit.METRE);
+		uLastHeight.setValue(BarometerReader.getHeight(lastValueMB));
+
+		// Create a bundle with last read (pressue)
+		Bundle bundlePressure = new Bundle();
+		bundlePressure.putInt(Widget.Intents.EXTRA_LAYOUT_REFERENCE, R.id.tvPressure);
+		bundlePressure.putString(Control.Intents.EXTRA_TEXT, uLastValue.toString());
+
+		// Create a bundle with last read value (height)
+		Bundle bundleHeight = new Bundle();
+		bundleHeight.putInt(Widget.Intents.EXTRA_LAYOUT_REFERENCE, R.id.tvHeight);
+		bundleHeight.putString(Control.Intents.EXTRA_TEXT, uLastHeight.toString());
+
+		Bundle[] layoutData = new Bundle[] { bundlePressure, bundleHeight };
+
+		// Send a UI when the widget is visible.
+		showLayout(R.layout.layout_widget, layoutData);
+	}
+
+	@Override
+	public void onStopRefresh() {
+		Log.d(HelloWidgetExtensionService.LOG_TAG, "stopRefesh");
+		if (baroReader != null) {
+			baroReader.stopReading();
+		}
+
+		Editor e = IntentTools.getSettingsEditor(mContext, BARO_WIDGET_SETTINGS);
+		e.putFloat(LAST_VALUE_MB, lastValueMB);
+		e.commit();
+		Log.d(HelloWidgetExtensionService.LOG_TAG, "saved " + lastValueMB);
+		pauseHandler();
+	}
+
+	@Override
+	public void onTouch(final int type, final int x, final int y) {
+		Log.d(HelloWidgetExtensionService.LOG_TAG, "onTouch() " + type);
+	}
+
+	@Override
+	public int getWidth() {
+		return (int) (mContext.getResources().getDimension(R.dimen.smart_watch_2_widget_cell_width) * WIDGET_WIDTH_CELLS);
+	}
+
+	@Override
+	public int getHeight() {
+		return (int) (mContext.getResources().getDimension(R.dimen.smart_watch_2_widget_cell_height) * WIDGET_HEIGHT_CELLS);
+	}
+
+	@Override
+	public int getPreviewUri() {
+		return R.drawable.widget_frame;
+	}
+
+	@Override
+	public int getName() {
+		return R.string.extension_name;
+	}
 }
